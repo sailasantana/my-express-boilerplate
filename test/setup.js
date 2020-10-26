8299999ff7e26becdeb8bfc76b2eb071c46d6069
sailasantana94 <sk4862@nyu.edu>
2020-10-26 12:50:27 -0400

    first commit

---
+++ test/setup.js
@@ -0,0 +1,11 @@
+
+//As you progress with libraries such as chai, 
+//you might want to install plugins that add 
+//features to chai. This file would be the ideal 
+//place to do that.
+
+const { expect } = require('chai')
+const supertest = require('supertest')
+
+global.expect = expect
+global.supertest = supertest
